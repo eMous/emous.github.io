45e559717be3fca05f393a9b7606fbbd2673df2e
devtsai <devtsai@outlook.com>
2019-03-31 22:01:51 +0800

    Table size bug fixed.

--- js/main.js
+++ js/main.js
@@ -55,3 +55,14 @@
 (function() {
   var demoItems = document.querySelectorAll('.grid-item')
 }());
+
+//////////////////////////table adapts the size//////////////////////////////
+(function() {
+  [].slice.call(document.querySelectorAll('table')).forEach(function(el){
+    var wrapper = document.createElement('div');
+    wrapper.className = 'table-area';
+    el.parentNode.insertBefore(wrapper, el);
+    el.parentNode.removeChild(el);
+    wrapper.appendChild(el);
+})
+}());
