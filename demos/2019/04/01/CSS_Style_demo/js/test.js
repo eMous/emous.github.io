cc06851660205a3bd1be6915bc2a26b2adf2af6c
devtsai <devtsai@outlook.com>
2019-04-04 14:05:29 +0800

    暂存

--- demos/2019/04/01/CSS_Style_demo/js/test.js
+++ demos/2019/04/01/CSS_Style_demo/js/test.js
@@ -1,1 +1,73 @@
-toString;
+'use strict';
+(() => {
+    const c = document.getElementById('c');
+    const ctx = c.getContext('2d');
+    const w = c.width = window.innerWidth;
+    const h = c.height = window.innerHeight;
+    const cx = w / 2;
+    const cy = h / 2;
+    const create3Points = (o, l) => {
+        const a = (Math.PI * 2) / 3;
+        const p = [];
+
+        for (let i = Math.PI / 2; i < (Math.PI * 2 + Math.PI / 2); i += a) {
+            p.push({
+                x: o.x + Math.cos(-i) * l,
+                y: o.y + Math.sin(-i) * l,
+            });
+        }
+
+        return p;
+    };
+    const draw = (ctx, p1, p2, d) => {
+        const dx = p2.x - p1.x;
+        const dy = p2.y - p1.y;
+        const dd = Math.sqrt(dx * dx + dy * dy);
+        const slice = dd / 3;
+        const aa = Math.atan2(dy, dx);
+        const a = {
+            x: p1.x + Math.cos(aa) * slice,
+            y: p1.y + Math.sin(aa) * slice,
+        };
+        const b = {
+            x: a.x + Math.cos(aa - Math.PI / 3) * slice,
+            y: a.y + Math.sin(aa - Math.PI / 3) * slice,
+        };
+        const c = {
+            x: a.x + Math.cos(aa) * slice,
+            y: a.y + Math.sin(aa) * slice,
+        };
+
+        if (d === 0) {
+            ctx.strokeStyle = 'white';
+            ctx.fillStyle = 'white';
+            ctx.beginPath();
+            ctx.moveTo(p1.x, p1.y);
+            ctx.lineTo(a.x, a.y);
+            ctx.lineTo(b.x, b.y);
+            ctx.lineTo(c.x, c.y);
+            ctx.lineTo(p2.x, p2.y);
+            ctx.closePath();
+            ctx.stroke();
+            ctx.fill();
+
+
+            return;
+        }
+
+        draw(ctx, p1, a, d - 1);
+        draw(ctx, a, b, d - 1);
+        draw(ctx, b, c, d - 1);
+        draw(ctx, c, p2, d - 1);
+    };
+
+    const points = create3Points({x: cx, y: cy}, h * 0.5).map((pt) => {
+        return { x: pt.x, y: pt.y + h * 0.1 };
+    });
+
+    const [p1, p2, p3] = points;
+
+    draw(ctx, p1, p2, 4);
+    draw(ctx, p2, p3, 4);
+    draw(ctx, p3, p1, 4);
+})();
--- demos/base/js/embed.js
+++ demos/base/js/embed.js
@@ -1,38 +1,57 @@
-
 var embeded_js = embeded_js || (function () {
-    var _args = {
+    (window.onload = function () {
+        (function initCSS(){
+            var dropdown_eles = document.getElementsByClassName("dropdown");
+            Array.from(dropdown_eles).forEach(function (el) {
+                el.addEventListener("mouseenter",function (event) {
+                    el.querySelector(".dropdown-content").style.display = "block";
+                })
+                el.addEventListener("mouseleave",function (event) {
+                    el.querySelector(".dropdown-content").style.display = "none";
+                })
+            });
+        }());
+    })
 
-    };
+
+    var _args = {};
+
     return {
         _args: _args,
         toggleFrameShow: function (_this, kind, href) {
+
             var active_eles = document.getElementsByClassName("active");
             var show_eles = document.getElementsByClassName("show");
 
             // Remove ALL active class label.
-            
-            Array.prototype.forEach.call(active_eles, function (el) {
+            Array.from(active_eles).forEach(function (el) {
                 el.classList.remove("active");
-                console.log(el.tagName);
             });
-            // [].forEach.call(active_eles, function (el) {
-            //     console.log(el.tagName);
-            // });
+
             // Make the ATag and its parent li active.
             _this.classList.add("active");
             _this.parentElement.parentElement.classList.add("active");
 
             // Remove ALL show class label.
-            Array.prototype.forEach.call(show_eles, function (el) {
+            Array.from(show_eles).forEach(function (el) {
                 el.classList.remove("show");
             });
+
             // Make ATag's kind viewer(result or html or js or css or others) show.
-            if (kind != "result" && kind != "html" && kind != "js" && kind != "css" && kind != "others") {
-                this._Log("Toggle an error kind of file: "+ kind);
+            if (kind != "result_viewer" && kind != "html_viewer" && kind != "js_viewer" && kind != "css_viewer" && kind != "others_viewer") {
+                this._Log("Toggle an error kind of file: " + kind);
                 return;
             }
-            document.getElementById(kind).classList.add("show");
 
+            // Make the clicked iframe visible.
+            var kind_container = document.getElementById(kind);
+            kind_container.classList.add("show");
+            var iframe = kind_container.querySelector("[src='" + href + "']");
+            iframe.classList.add("show");
+
+            // Hide the dropdown-container.
+            const mouse_levea_event = new Event('mouseleave');
+            _this.parentElement.parentElement.dispatchEvent(mouse_levea_event);
         },
         _getResultPageHeight: function (id) {
             try {
@@ -94,44 +113,26 @@ var embeded_js = embeded_js || (function () {
                 // result.style.height = result_iframe.style.height
             }());
             (function analyseDemoFiles() {
+                document.querySelector("#actions").style.display = "block";
+
                 var args = _this._args
                 var html_files = args.this_demo_files.html;
                 var js_files = args.this_demo_files.js;
                 var css_files = args.this_demo_files.css;
+                var others_files = args.this_demo_files.others;
 
                 var a_li_result = document.querySelector("a[href='#Result']").parentElement;
                 var a_li_html = document.querySelector("a[href='#HTML']").parentElement;
                 var a_li_js = document.querySelector("a[href='#JavaScript']").parentElement;
+                var a_li_css = document.querySelector("a[href='#CSS']").parentElement;
+                var a_li_others = document.querySelector("a[href='#Others']").parentElement;
 
                 var getFileName = function (str) {
                     var index = str.lastIndexOf("/");
                     return str.substr(index + 1);
                 }
-                var createAnResultIframeToContainer = function (container, html_url) {
-                    uriEmbedded = html_url;
-                    iframe = document.createElement("iframe");
-                    iframe.src = uriEmbedded;
-                    iframe.id = html_url;
-                    iframe.scrolling = "auto";
-                    iframe.width = "100%";
-                    iframe.height = _this._args.demo_iframe.offsetHeight - document.querySelector("header").offsetHeight;
-                    iframe.frameBorder = "0";
-                    iframe.allowtransparency = true;
-                    iframe.sandbox = "allow-modals allow-forms allow-popups allow-scripts allow-same-origin";
-                    container.insertBefore(iframe, container.firstChild);
-                    return iframe;
-                }
-
-                if (html_files.length == 0) {
-                    // Means that there is no result page. 
-                    a_li_result.classList.remove("active");
-                    a_li_js.classList.add("active");
-
-                    a_li_result.style.display = "none";
-                    a_li_html.style.display = "none";
-                } else {
-                    var a_li_dropdown_content = a_li_result.getElementsByClassName("dropdown-content")[0];
-                    var length = a_li_dropdown_content.children.length - html_files.length;
+                var insertATagsToDropdownContent = function(a_li_dropdown_content,arr_files,kind){
+                    var length = a_li_dropdown_content.children.length - arr_files.length;
                     // Dynamic add ATag.
                     if (length > 0) {
                         for (var i = 0; i < length; i++) {
@@ -146,27 +147,106 @@ var embeded_js = embeded_js || (function () {
                         }
                     }
                     // Set ATags' attrs.
-                    for (var i = 0; i < html_files.length; i++) {
-                        a_li_dropdown_content.children[i].textContent = getFileName(html_files[i]);
-                        a_li_dropdown_content.children[i].setAttribute("href", "#" + html_files[i]);
+                    for (var i = 0; i < arr_files.length; i++) {
+                        a_li_dropdown_content.children[i].textContent = getFileName(arr_files[i]);
+                        a_li_dropdown_content.children[i].setAttribute("href", "#" + arr_files[i]);
                         var quota = "\"";
-                        a_li_dropdown_content.children[i].setAttribute("onclick", "embeded_js.toggleFrameShow(" + "this" + "," + quota + "result" + quota + "," + quota + html_files[i] + quota + ")");
-                        // Make the first active.
-                        if (i == 0) {
-                            a_li_dropdown_content.children[i].classList.add("active");
-                        }
-                    }
-
-                    // Create iframes.
-                    for (var i = 0; i < html_files.length; i++) {
-                        var iframe = createAnResultIframeToContainer(document.getElementById("result"), html_files[i]);
-                        // Make other iframes hidden.
-                        if (i != 0) {
-                            iframe.style.display = "none";
-                        }
+                        a_li_dropdown_content.children[i].setAttribute("onclick", "embeded_js.toggleFrameShow(" + "this" + "," + quota + kind + quota + "," + quota + arr_files[i] + quota + ")");
                     }
                 }
-                ;
+                var createAnResultIframeToContainer = function (container, html_url) {
+                    uriEmbedded = html_url;
+                    iframe = document.createElement("iframe");
+                    iframe.src = uriEmbedded;
+                    iframe.scrolling = "auto";
+                    iframe.width = "100%";
+                    iframe.height = _this._args.demo_iframe.offsetHeight - document.querySelector("header").offsetHeight;
+                    iframe.frameBorder = "0";
+                    iframe.allowtransparency = true;
+                    iframe.sandbox = "allow-modals allow-forms allow-popups allow-scripts allow-same-origin";
+                    container.insertBefore(iframe, container.firstChild);
+                    return iframe;
+                }
+                // Result dropdown_content.
+                if (html_files.length == 0) {
+                    // Means that there is no result page. 
+                    a_li_result.classList.remove("active");
+                    a_li_js.classList.add("active");
+
+                    a_li_result.style.display = "none";
+                    a_li_html.style.display = "none";
+                } else {
+                    var a_li_dropdown_content = a_li_result.getElementsByClassName("dropdown-content")[0];
+                    insertATagsToDropdownContent(a_li_dropdown_content,html_files,"result_viewer");
+                    // Create iframes.
+                    for (var i = 0; i < html_files.length; i++) {
+                        var iframe = createAnResultIframeToContainer(document.getElementById("result_viewer"), html_files[i]);
+                    }
+
+                    // Show the first demo.
+                    _this.toggleFrameShow(a_li_dropdown_content.children[0], "result_viewer", a_li_dropdown_content.children[0].getAttribute("href").substr(1))
+                }
+                // Javascript dropdown_content.
+                if (js_files.length == 0){
+                    a_li_js.style.display = "none";
+                    if (html_files.length == 0) {
+                        // Means that there is no result page and no js page.
+                        a_li_js.classList.remove("active");
+                        a_li_css.classList.add("active");
+                    }
+                }else{
+                    var a_li_dropdown_content = a_li_js.getElementsByClassName("dropdown-content")[0];
+                    insertATagsToDropdownContent(a_li_dropdown_content,js_files,"js_viewer");
+
+                    // Create iframes.
+                    for (var i = 0; i < js_files.length; i++) {
+                        var iframe = createAnResultIframeToContainer(document.getElementById("js_viewer"), js_files[i]);
+                    }
+                }
+                // CSS dropdown_content.
+                if (css_files.length == 0){
+                    a_li_css.style.display = "none";
+                    if (js_files.length == 0 && html_files.length == 0) {
+                        // Means that there is no result page and no js page.
+                        a_li_css.classList.remove("active");
+                        a_li_others.classList.add("active");
+                    }
+                }else{
+                    var a_li_dropdown_content = a_li_css.getElementsByClassName("dropdown-content")[0];
+                    insertATagsToDropdownContent(a_li_dropdown_content,css_files,"css_viewer");
+
+                    // Create iframes.
+                    for (var i = 0; i < css_files.length; i++) {
+                        var iframe = createAnResultIframeToContainer(document.getElementById("css_viewer"), css_files[i]);
+                    }
+                }
+                // HTML dropdown_content.
+                if (html_files.length != 0) {
+                    var a_li_dropdown_content = a_li_html.getElementsByClassName("dropdown-content")[0];
+                    insertATagsToDropdownContent(a_li_dropdown_content, html_files, "html_viewer");
+                    // Create iframes.
+                    for (var i = 0; i < html_files.length; i++) {
+                        var iframe = createAnResultIframeToContainer(document.getElementById("html_viewer"), html_files[i]);
+                    }
+                }
+                // Others dropdown_content.
+                if (others_files.length == 0){
+                    a_li_others.style.display = "none";
+                    if (css_files.length == 0 && js_files.length == 0 && html_files.length == 0) {
+                        // Means that there is no result page and no js page.
+                        a_li_others.classList.remove("active");
+                        _this._Log("There is empty in the demo.");
+                        return;
+                    }
+                }else{
+                    var a_li_dropdown_content = a_li_others.getElementsByClassName("dropdown-content")[0];
+                    insertATagsToDropdownContent(a_li_dropdown_content,others_files,"others_viewer");
+
+                    // Create iframes.
+                    for (var i = 0; i < others_files.length; i++) {
+                        var iframe = createAnResultIframeToContainer(document.getElementById("others_viewer"), others_files[i]);
+                    }
+                }
             }())
 
             // var s = this._getOverflowSettingOfResultPage()
